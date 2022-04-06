570ca4e972edaadd413f4654a4c3af862c39f31b
Solon <amaral.solon@hotmail.com>
2022-04-06 18:10:21 -0300

    exercicios 4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercicio-primeiros-passos-tipos-primitivos-tipagem-operacoes.js
@@ -0,0 +1,20 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientAge)
+console.log(typeof patientId)
+
+// exercicio abaixo
+
+const base = 5
+let height = 8
+const area = base * height
+const perimeter = base + (height * 2)
+
+console.log(area)
+console.log(perimeter)
